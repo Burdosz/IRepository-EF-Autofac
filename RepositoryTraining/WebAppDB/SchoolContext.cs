addd8b37b26a35b67c6a202dfa468688ebbf0580
DESKTOP-B7MQG91\Bartek <bartosz.burdosz>
2017-04-17 00:31:09 +0200

    Initial commit

---
+++ RepositoryTraining/WebAppDB/SchoolContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppDB.Models;
+
+namespace WebAppDB
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name=SchoolDBConnectionString")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+
+    }
+
+    public class TestDB
+    {
+        public static void UseDB()
+        {
+            using(var ctx = new SchoolContext())
+            {
+                Student stud = new Student() { StudentName = "New Student22" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
